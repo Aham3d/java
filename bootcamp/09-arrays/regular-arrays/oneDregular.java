59553714a2cc608d04b3421b61cb9d008a90fac3
Moin Ahamed <ahamedjust@gmail.com>
2023-08-17 16:18:45 +0530

    1D & 2D examples

--- bootcamp/09-arrays/regular-arrays/oneDregular.java
+++ bootcamp/09-arrays/regular-arrays/oneDregular.java
@@ -1,20 +1,17 @@
-package java;
+// an array to store the ages of 5 students 
 
 import java.util.Scanner;
 
 public class oneDregular {
-	
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in); 
+		Scanner sc = new Scanner(System.in);
 		int[] a = new int[5]; 
-		
+
 		for(int i = 0; i < a.length; i++) {
-			System.out.println("enter the age"); 
-			a[i] = sc.nextInt(); 
+			System.out.println("enter the age");
+			a[i] = sc.nextInt();
 		}
-		
-		System.out.println("the ages are"); 
-		
+
 		for(int i = 0; i < a.length; i++) {
 			System.out.println(a[i]);
 		}
---
+++ bootcamp/09-arrays/regular-arrays/twoDregular.java
@@ -0,0 +1,23 @@
+// an array to store the ages of students belonging to 2 classrooms with 5 students each
+import java.util.Scanner;
+
+public class twoDregular {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int[][] a = new int[2][5];
+    
+    for(int i = 0; i < a.length; i++) {
+      for(int j = 0; j < a[i].length; j++) {
+        System.out.println("enter the age for classroom"+i + "student"+j);
+        a[i][j] = sc.nextInt();
+      }
+    }
+
+        for(int i = 0; i < a.length; i++) {
+      for(int j = 0; j < a[i].length; j++) {
+        System.out.println(a[i][j]);
+      }
+    }
+  }
+}
