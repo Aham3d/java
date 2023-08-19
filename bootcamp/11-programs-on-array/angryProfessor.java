8d6a2ad12aa83fdbfe7676e3aacc7955718c9313
Moin Ahamed <ahamedjust@gmail.com>
2023-08-19 18:19:17 +0530

    programs on array - angry professor

---
+++ bootcamp/11-programs-on-array/angryProfessor.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class angryProfessor {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in); 
+    int n = sc.nextInt(); 
+    int k = sc.nextInt(); 
+
+    int[] a = new int[n]; 
+
+    for(int i = 0 ; i < a.length; i++) {
+      a[i] = sc.nextInt();      
+    }
+    System.out.println(angryProfessor(k, a));
+  }
+
+  static String angryProfessor(int k, int[] a) {
+    int count = 0; 
+
+    for(int i = 0; i < a.length; i++) {
+      if(a[i] <= 0) {
+        count++;
+      }
+    }
+
+    if(count >= k) {
+      return "no"; 
+    } else {
+      return "yes"; 
+    }
+  }
+
+}
