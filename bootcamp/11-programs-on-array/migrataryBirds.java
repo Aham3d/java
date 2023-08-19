489a39a354938260d395502cab8a7e764048fedd
Moin Ahamed <ahamedjust@gmail.com>
2023-08-19 18:37:34 +0530

    progams on array - migratory birds, walkWidth

---
+++ bootcamp/11-programs-on-array/migrataryBirds.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class migrataryBirds {
+  
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in); 
+    int n = sc.nextInt();
+    int[] a = new int[n];
+    // int[] a = {1, 2, 2, 3, 4,}; 
+
+    for(int i = 0; i < a.length; i++) {
+      a[i] = sc.nextInt(); 
+    }
+
+    System.out.println(migrataryBirds(a));
+  }
+
+  static int   migrataryBirds(int[] a) {
+    int[] birds = new int[a.length]; 
+
+    for(int i = 0; i < a.length; i++) {
+      birds[a[i]]++; 
+    }
+
+    int maxIndex = 0, max = -1; 
+
+    for(int i = 1; i < birds.length; i++) {
+      if(birds[i] > max) {
+        max = birds[i]; 
+        maxIndex = i; 
+      }
+    }
+    return maxIndex; 
+  }
+
+}
---
+++ bootcamp/11-programs-on-array/walkWidth.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class walkWidth {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in); 
+    int n = sc.nextInt(); 
+    int h = sc.nextInt(); 
+    int [] a = new int[n]; 
+
+    for(int i = 0; i < a.length; i++) {
+      a[i] = sc.nextInt(); 
+    }
+    System.out.println(walkWidth(a, h));
+  }
+
+  static int walkWidth(int[] a, int h) {
+    int width = 0; 
+    for(int i = 0; i < a.length; i++) {
+      if(a[i] <= h) {
+        width += 1; 
+      } else {
+        width += 2; 
+      }
+    }
+    return width; 
+  }
+}
