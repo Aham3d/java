9b686d6f605ec22a2f602ccc99be2eb555a88a69
Moin Ahamed <ahamedjust@gmail.com>
2023-08-30 16:41:06 +0530

    endsWith()

---
+++ bootcamp/12-strings/built-in-2/endsWith.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class endsWith {
+  
+  static boolean endsWith(String s, String t) {
+    int count = 0; 
+
+    for(int i = s.length() - t.length(); i < s.length(); i++) {
+      if(s.charAt(i) == t.charAt(count)) {
+        count++; 
+      }
+    }
+
+    if(t.length() == count) {
+      return true; 
+    } else {
+      return false; 
+    }
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in); 
+
+    String s = sc.nextLine(); 
+    String t = sc.nextLine(); 
+
+    System.out.println(endsWith(s , t));
+  }
+
+}
