c89e3096d9bd1863dddcea23815128b0275df830
Moin Ahamed <ahamedjust@gmail.com>
2023-08-30 16:25:05 +0530

    toCharArray()

---
+++ bootcamp/12-strings/built-in-2/toCharArray.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class toCharArray {
+  
+  static char[] toCharArray(String s) {
+    char[] c = new char[s.length()]; 
+
+    for(int i = 0; i < s.length(); i++) {
+      c[i] = s.charAt(i); 
+    }
+    return c; 
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in); 
+    
+    String s = sc.nextLine(); 
+
+    System.out.println(toCharArray(s));
+  }
+
+}
