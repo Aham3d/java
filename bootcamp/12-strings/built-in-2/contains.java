f284bcfec3346b05034bfbc4b5dc6086489c47a5
Moin Ahamed <ahamedjust@gmail.com>
2023-08-31 21:42:16 +0530

    contains()

---
+++ bootcamp/12-strings/built-in-2/contains.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class contains {
+
+  static boolean contains(String s, String t) {
+    int count = 0; 
+
+    for(int i = 0; i < s.length() && count<t.length(); i++) {
+      if(s.charAt(i) == t.charAt(count)) {
+        count++; 
+      } else {
+        count = 0; 
+      }
+    }
+
+    if(t.length() == count) {
+      return true; 
+    } else {
+      return false; 
+    }
+  }
+
+  public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in); 
+    String s = sc.nextLine(); 
+    String t = sc.nextLine(); 
+
+
+    System.out.println(contains(s, t));
+
+  }
+
+}
