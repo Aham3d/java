0133c1df2c6e1ecd7d14518eeaf63d0098bbd484
Moin Ahamed <ahamedjust@gmail.com>
2023-09-13 21:44:05 +0530

    method overloading

---
+++ bootcamp/13-oops/intro.java
@@ -0,0 +1,11 @@
+public class intro {
+  
+  public static void main(String[] args) {
+    /*
+     * world is a collection of objects
+     * every object belongs to a type
+     * every object contains two part(had , does) 
+     */
+  }
+
+}
---
+++ bootcamp/13-oops/methodOverloading.java
@@ -0,0 +1,43 @@
+public class methodOverloading {
+  
+  public static void main(String[] args) {
+    
+    /*
+     * can use same names for methods
+     * ease of use
+     * 
+     * to resolve the naming conflict:
+     * 1. no of parameters
+     * 2. type of parameters
+     * 3. order of type of parameters
+     * 
+     * method overloading also helps to achieve virtual polymorphism
+     * 
+     * nothing is overloaded in method overloading. it is just and illusion that one method is doing many things, 
+     * but in reality various methods are there doing one thing each.
+     */
+
+    methodOverloading m = new methodOverloading(); 
+    m.print(10.5f, 10); 
+    m.print(10); 
+    m.print(10.4f);
+        
+  }
+
+  void print(int a) {
+    System.out.println(a);
+  }
+
+  void print(float a) {
+    System.out.println(a);
+  }
+
+  // void print(float a, int b) {
+  //   System.out.println(a, b);
+  // }
+
+  // void print(int a, float b) {
+  //   System.out.println(a,b);
+  // }
+
+}
