9a74ca7d39e063f531c1579d57ea2edca8d18ad7
Moin Ahamed <ahamedjust@gmail.com>
2023-09-14 14:59:44 +0530

    encapsulation and constructor

---
+++ bootcamp/13-oops/encapsulation, shadowing and constructor/constructor/constructor.java
@@ -0,0 +1,40 @@
+/*
+ * it is a specialized, parameterzed setData
+ * constructor have the same name as the className
+ * they don't have return type
+ * they are called during abject craetion
+ */
+public class constructor {
+  
+  public static void main(String[] args) {
+    customer c = new customer(001, "moin", 989798798); 
+
+    System.out.println(c.getcId() + " " + c.getcName() + " " + c.getcNumber());   
+  }
+  
+}
+
+class customer {
+  private int cID; 
+  private String cName; 
+  private long cNumber; 
+
+  public customer(int cID, String cName, long cNumber) {
+    this.cID = cID; 
+    this.cName = cName; 
+    this.cNumber = cNumber; 
+  }
+
+  public int getcId() {
+    return cID; 
+  }
+
+  public String getcName() {
+    return cName; 
+  }
+
+  public long getcNumber() {
+    return cNumber; 
+  }
+
+}
