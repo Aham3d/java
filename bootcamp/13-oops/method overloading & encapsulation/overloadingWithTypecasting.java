79f8034975a954fc67e8dfcb2f4e62c138b4e596
Moin Ahamed <ahamedjust@gmail.com>
2023-09-14 13:56:44 +0530

    method overloading with typecasting(implicit))

---
+++ bootcamp/13-oops/method overloading & encapsulation/overloadingWithTypecasting.java
@@ -0,0 +1,21 @@
+public class overloadingWithTypecasting {
+  
+  public static void main(String[] args) {
+     calculate c = new calculate(); 
+     
+     System.out.println(c.add(10, 20));
+  }
+
+}
+
+class calculate {
+  double add(int a, float b) { // executes this method
+    System.out.println("1");
+    return a+b; 
+  }
+
+  double add(float a, float b) {
+    System.out.println("2");
+    return a+b;
+  }
+}
