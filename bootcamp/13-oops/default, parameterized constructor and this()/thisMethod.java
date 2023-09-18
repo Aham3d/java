b363ba8989debc5fc2ae6a533faf7033c4de5ac1
Moin Ahamed <ahamedjust@gmail.com>
2023-09-18 20:53:59 +0530

    this method example 1

---
+++ bootcamp/13-oops/default, parameterized constructor and this()/thisMethod.java
@@ -0,0 +1,39 @@
+class customer2 {
+  
+  private int cId;
+  private String cName; 
+  private long cNumber; 
+  
+  public customer2() {
+    cId = 102; 
+    cName = "name2"; 
+    cNumber = 4987434344L;
+  }
+
+  public customer2(int cId, String cName, long cNumber) {
+    this(); 
+  }
+
+  public int getCid() {
+    return cId; 
+  }
+
+  public String getCname() {
+    return cName; 
+  }
+
+  public long getNumber() {
+    return cNumber; 
+  }
+
+}
+
+public class thisMethod {
+
+  public static void main(String[] args) {
+    customer2 c = new customer2(101, "name1", 398479857349L); 
+
+    System.out.println(c.getCid() + " " + c.getCname() + " " + c.getNumber());
+  }
+
+}
